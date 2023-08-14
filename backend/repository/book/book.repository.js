5d2525f94811e2cb8c0bac3f52c9c4d6ad8af351
Nilanjan Bhattacharya <98635795+nbhattacharya08@users.noreply.github.com>
2023-08-14 14:30:33 +0530

    Update book.repository.js

--- backend/repository/book/book.repository.js
+++ backend/repository/book/book.repository.js
@@ -4,6 +4,12 @@ const getBooks = async () => {
 	return await Book.find({});
 };
 
+const addBook = async (bookdetails) =>{
+	const newBook = new Book(bookdetails)
+	const res = await newBook.save()
+	return res
+};
+
 module.exports = {
-	getBooks,
+	getBooks,addBook
 };
