f9e35c665e32746c7422e54f80f59fbbb6af22ea
Nilanjan Bhattacharya <98635795+nbhattacharya08@users.noreply.github.com>
2023-08-14 14:34:46 +0530

    Update book.route.js

--- backend/controller/book.route.js
+++ backend/controller/book.route.js
@@ -9,4 +9,14 @@ router.get("/", async (req, res, next) => {
 	}
 });
 
+router.get("/add" , async (req,res) => {
+	try{
+		const bookdetails = req.body 
+		const response = bookRepository.addBook(bookdetails)
+		res.status(200).json(response)
+	}catch(e){
+		res.status(500).send("book not added")
+	}
+}
+
 module.exports = router;
